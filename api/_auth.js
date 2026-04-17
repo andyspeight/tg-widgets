1d08bd685ca4105bceee1ecc700c885087a45fbd
andyspeight <andy.speight@agendas.group>
2026-04-17 16:09:05 +0100

    Update _auth.js

--- api/_auth.js
+++ api/_auth.js
@@ -211,6 +211,6 @@ export function isValidEmail(email) {
 
 export function setCors(res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 }
